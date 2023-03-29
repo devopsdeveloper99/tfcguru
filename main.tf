89807dab422be76215c565002ed2ee63ca08f3f2
devopsdeveloper99 <devopsdeveloper99@gmail.com>
2023-03-29 03:28:44 -0400

    gg

--- main.tf
+++ main.tf
@@ -1,9 +1,56 @@
 provider "google" {
-    project = "playground-s-11-0d36a919"
+    project = "playground-s-11-e800175f"
     region = "us-central1-a"
 }
 
-resource "google_storage_bucket" "static-site" {
-  name          = "naztest8789065536633773700"
-  location      = "US-CENTRAL1"
+# resource "google_storage_bucket" "static-site" {
+#   name          = "naztest8789065536633773700"
+#   location      = "US-CENTRAL1"
+# }
+
+resource "google_service_account" "default" {
+  account_id   = "250601101404-compute@developer.gserviceaccount.com"
+  display_name = "Compute Engine default service account"
+}
+
+resource "google_compute_instance" "default" {
+  name         = "test"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+
+  tags = ["foo", "bar"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      labels = {
+        my_label = "value"
+      }
+    }
+  }
+
+  // Local SSD disk
+  scratch_disk {
+    interface = "SCSI"
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+
+  metadata = {
+    foo = "bar"
+  }
+
+  metadata_startup_script = "echo hi > /test.txt"
+
+  service_account {
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    email  = google_service_account.default.email
+    scopes = ["cloud-platform"]
+  }
 }
