2ab09d96c3f2c9fa2e746339847d8dc0eb47aca5
devopsdeveloper99 <devopsdeveloper99@gmail.com>
2023-03-29 04:01:43 -0400

    gg

--- main.tf
+++ main.tf
@@ -1,56 +1,32 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.59.0"
+    }
+  }
+}
+
 provider "google" {
-    project = "playground-s-11-e800175f"
-    region = "us-central1-a"
+  project = "playground-s-11-e800175f"
+  region  = "us-central1"
+  zone    = "us-central1-c"
 }
 
-# resource "google_storage_bucket" "static-site" {
-#   name          = "naztest8789065536633773700"
-#   location      = "US-CENTRAL1"
-# }
-
-resource "google_service_account" "default" {
-  account_id   = "116484940409849554682"
-  display_name = "Compute Engine default service account"
-}
-
-resource "google_compute_instance" "default" {
-  name         = "test"
-  machine_type = "e2-medium"
-  zone         = "us-central1-a"
-
-  tags = ["foo", "bar"]
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "e2-micro"
 
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-11"
-      labels = {
-        my_label = "value"
-      }
     }
-  }
-
-  // Local SSD disk
-  scratch_disk {
-    interface = "SCSI"
   }
 
   network_interface {
+    # A default network is created for all GCP projects
     network = "default"
-
     access_config {
-      // Ephemeral public IP
     }
-  }
-
-  metadata = {
-    foo = "bar"
-  }
-
-  metadata_startup_script = "echo hi > /test.txt"
-
-  service_account {
-    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
-    email  = google_service_account.default.email
-    scopes = ["cloud-platform"]
   }
 }
